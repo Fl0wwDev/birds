6e5c4b3d9ee9eda362af5809b90ffcbab5c4e0b9
Fl0wwDev <mafradin@hotmail.fr>
2025-11-07 11:31:40 +0100

    Add BirdModel class with common and scientific name attributes
    - Implemented BirdModel class to represent bird species with common and scientific names.
    - Added getter and setter methods for both attributes.
    - Included a toString method for easy representation of BirdModel instances.
    - The class is designed to facilitate the management of bird data in the application.

--- src/main/java/birds/ensisa/BirdApplication.java
+++ src/main/java/birds/ensisa/BirdApplication.java
@@ -19,7 +19,7 @@ public class BirdApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(
         BirdApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Des oiseaux");
         stage.setScene(scene);
         stage.show();
---
+++ src/main/java/ensisa/birds/model/BirdModel.java
@@ -0,0 +1,42 @@
+package ensisa.birds.model;
+
+/**
+ * Placeholder POJO for the package `ensisa.birds.model`.
+ * This file exists to create the package and provide a minimal class to use.
+ */
+public class BirdModel {
+    private String commonName;
+    private String scientificName;
+
+    public BirdModel() {
+    }
+
+    public BirdModel(String commonName, String scientificName) {
+        this.commonName = commonName;
+        this.scientificName = scientificName;
+    }
+
+    public String getCommonName() {
+        return commonName;
+    }
+
+    public void setCommonName(String commonName) {
+        this.commonName = commonName;
+    }
+
+    public String getScientificName() {
+        return scientificName;
+    }
+
+    public void setScientificName(String scientificName) {
+        this.scientificName = scientificName;
+    }
+
+    @Override
+    public String toString() {
+        return "BirdModel{" +
+                "commonName='" + commonName + '\'' +
+                ", scientificName='" + scientificName + '\'' +
+                '}';
+    }
+}
